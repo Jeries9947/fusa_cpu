8db3ae4a3f0e8170b97f58ab79eae4f41441c8f7
natalieGha <56216645+natalieGha@users.noreply.github.com>
2025-12-11 23:12:25 +0200

    Update sign_extend.v

--- rtl/sign_extend.v
+++ rtl/sign_extend.v
@@ -1,7 +1,10 @@
-// sign_extend.v
 module sign_extend (
     input  wire [15:0] imm,
+    input  wire        imm_unsigned,   // 1: zero-extend, 0: sign-extend
     output wire [31:0] imm_ext
 );
-    assign imm_ext = {{16{imm[15]}}, imm};
+    assign imm_ext =
+        imm_unsigned ? {16'b0, imm} :          // zero-extend
+                       {{16{imm[15]}}, imm};    // sign-extend
 endmodule
+
